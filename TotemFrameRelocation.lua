da46cb6b96b84fa8fbe936efc37c63c87b1c952f
Zaazu <rb@zaazu.dev>
2024-07-21 04:44:42 +0100

    Added max parenting attempts before giving up

--- TotemFrameRelocation.lua
+++ TotemFrameRelocation.lua
@@ -1,32 +1,56 @@
-local ParentFrameName = "TargetFrame"        -- Frame for the TotemFrame to attach to.
+local ParentFrameName = "awdaw"        -- Frame for the TotemFrame to attach to.
 local ParentAnchorPosition = "BOTTOMRIGHT";  -- Attachment point on the parent frame.
 local TotemFrameAnchorPosition = "TOPRIGHT"; -- Attachment point on the TotemFrame.
 local UseSquareMask = false;                 -- Apply square mask to totem icons (true or false).
 local XOffset = 0;                           -- Horizontal offset from the attachment point.
 local YOffset = 0;                           -- Vertical offset from the attachment point.
 
-local verbose = true
+
+local MaxParentAttempts = 10;
+local ParentAttempt = 0
+local Disabled = false;
+local Verbose = true
 
 local function Printv(message)
-	if(verbose) then
-		print(message)
+	if(Verbose) then
+		print("[TotemFrameRelocation]: " .. message)
 	end
 end
 
 local function ReparentFrame(self)
+	print(ParentAttempt)
+	if(Disabled) then
+		return
+	end
+
 	local ParentFrame = _G[ParentFrameName]
+
+	--Give up if it takes too long to parent
+	if(ParentAttempt >= MaxParentAttempts) then
+		Printv(ParentFrameName .. " frame does not exist, giving up.")
+		
+		Disabled = true
+		return
+	end
 	
+	--If the frame to parent exists and we're not attached to it yet
 	if(ParentFrame and self:GetParent() ~= ParentFrame) then
+		Printv("Attached " .. self:GetName() .. " to " .. ParentFrame:GetName() .. ".")
+
 		self:ClearAllPoints()
 		self:SetParent(ParentFrame)
 		self:SetPoint(TotemFrameAnchorPosition, ParentFrame, ParentAnchorPosition, XOffset, YOffset)
 		
-		Printv("Attached " .. self:GetName() .. " to " .. ParentFrame:GetName())
+		ParentAttempt = 0
 	else
+		--If we're still not attached, the frame doesn't exist
 		if(self:GetParent() ~= ParentFrame) then
-			C_Timer.After(1, ReparentFrame)
-			
-			Printv("frame not found")
+			Printv(ParentFrameName .. " frame not found, retrying.")
+
+			ParentAttempt = ParentAttempt + 1
+			C_Timer.After(1, function()
+				ReparentFrame(self)
+			end)
 		end
 	end
 end
